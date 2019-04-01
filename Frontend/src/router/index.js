343b9da48fe81fd4d5292aabaf9defb80ff422e5
EC2 Default User <ec2-user@ip-172-31-1-156.ec2.internal>
2019-04-01 13:24:21 +0000

    Fix redirection and move .gitignore

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,34 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import store from '../store'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 import login from '@/components/login'
 import tcl from '@/components/tcl'
 
 Vue.use(Router)
 
-let entryUrl = null
+// let entryUrl = null
 
-const guard = async (to, from, next) => {
-  if (store.state.signedIn) {
-    if (entryUrl) {
-      const url = entryUrl
-      entryUrl = null
-      return next(url) // goto stored url
-    } else {
-      return next() // all is fine
-    }
-  }
-
-  await store.dispatch('checkAuth')
-  // we use await as this async request has to finish
-  // before we can be sure
-
+const guardTcl = async (to, from, next) => {
   if (store.state.signedIn) {
     next()
   } else {
-    entryUrl = to.path // store entry url before redirect
     next('/login')
+  }
+}
+
+const guardLogin = async (to, from, next) => {
+  if (store.state.signedIn) {
+    next('/tcl')
+  } else {
+    next()
   }
 }
 
@@ -36,17 +29,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'root',
+      redirect: '/tcl'
     },
     {
       path: '/login',
+      beforeEnter: guardLogin,
       name: 'login',
       component: login
     },
     {
       path: '/tcl',
-      beforeEnter: guard,
+      beforeEnter: guardTcl,
       name: 'tcl',
       component: tcl
     }
