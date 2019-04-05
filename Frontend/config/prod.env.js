5f5217688462e4dbb558d8f73b6731f8f255351c
Benjamin EHLERS <Benjamin.EHLERS@hardis.fr>
2019-04-05 17:17:04 +0200

    Prod variabilize

--- Frontend/config/prod.env.js
+++ Frontend/config/prod.env.js
@@ -1,5 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_BUILD_SIGNED_URL_ENDPOINT: '"https://o7b2gdvqsd.execute-api.us-east-1.amazonaws.com/prod/buildSignedUrlUpload"'
+  VUE_APP_BUILD_SIGNED_URL_ENDPOINT: '"https://o7b2gdvqsd.execute-api.us-east-1.amazonaws.com/prod/buildSignedUrlUpload"',
+  AWS_AUTHORIZER: '"https://o7b2gdvqsd.execute-api.us-east-1.amazonaws.com/prod"',
+  AWS_USER_POOL_ID: '"us-east-1_N9HQPnrAW"',
+  AWS_USER_POOL_WEB_CLIENT_ID: '"1eof6tji2famvd43d8v2cd45vo"'
 }
